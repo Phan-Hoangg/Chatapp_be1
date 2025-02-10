091f9e444f7371328d5fb6d259a00daa392ddf63
Phan Hoang <phanhoang4504@gmail.com>
2025-02-10 17:46:37 +0700

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 //env variables
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
-  console.log(`server is listening at ${PORT}`);
+  logger.info(`Server is listening at ${PORT}`);
 });
