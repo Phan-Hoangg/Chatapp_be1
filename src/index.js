ab15e43cf85384d1e5e557b335671c1b483e0b6a
Phan Hoang <phanhoang4504@gmail.com>
2025-02-10 17:26:05 +0700

    setup cross-env, adding middlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 
-//dotEnv config
-dotenv.config();
-
 //env variables
 const PORT = process.env.PORT || 8000;
-
+console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
   console.log(`server is listening at ${PORT}`);
 });
